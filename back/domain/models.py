ff7e93f71b16d00d232cff5b1f2ac27e9b804d9b
DennerBismarck <dennerbismarck@gamil.com>
2025-04-03 15:30:54 -0300

    Feat: Criando dominio da classe pessoa #2

---
+++ back/domain/models.py
@@ -0,0 +1,11 @@
+class pessoa():
+    def __init__(self, cpf, nome, email, telefone, senha):
+        self._cpf = cpf
+        self._nome = nome
+        self._email = email
+        self._telefone = telefone
+        self._senha = senha
+
+
+        
+    
