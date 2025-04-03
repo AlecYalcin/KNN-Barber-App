37d0dc027775dd23f8387f5494de5266a8667660
DennerBismarck <dennerbismarck@gamil.com>
2025-04-03 15:36:18 -0300

    Feat: Criando dominio da classe barbeiro #2

--- back/domain/models.py
+++ back/domain/models.py
@@ -6,6 +6,8 @@ class pessoa():
         self._telefone = telefone
         self._senha = senha
 
-
-        
+class barbeiro(pessoa):
+    def __init__(self, cpf, nome, email, telefone, senha, tel_trabalho):
+        super().__init__(cpf, nome, email, telefone, senha)
+        self._tel_trabalho = tel_trabalho
     
