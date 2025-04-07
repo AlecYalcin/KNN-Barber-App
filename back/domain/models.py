d8212b25a0563283d41d5e4ee4ca3dc0081b599c
DennerBismarck <dennerbismarck@gamil.com>
2025-04-07 08:56:52 -0300

    Feat: Reestruturando código para deixar mais legível, atualizando coisas que Alec Nariz pediu e criando função de cadastrar horário de atendimento #2

--- back/domain/models.py
+++ back/domain/models.py
@@ -1,32 +1,27 @@
+import uuid
 from enum import Enum
 
-# Enums para padronizar status
+# ----------------------------- ENUMs -----------------------------
 class StatusServico(Enum):
-    AGENDADO = "Agendado"
-    CONCLUIDO = "Concluído"
-    CANCELADO = "Cancelado"
+    AGENDADO = 'Agendado'
+    CONFIRMADO = 'Confirmado'
+    CANCELADO = 'Cancelado'
 
 class StatusPagamento(Enum):
-    PENDENTE = "Pendente"
-    PAGO = "Pago"
-    CANCELADO = "Cancelado"
+    PENDENTE = 'Pendente'
+    CONCLUIDO = 'Concluído'
+    FALHOU = 'Falhou'
 
-class Disponibilidade(Enum):
-    DISPONIVEL = "Disponível"
-    INDISPONIVEL = "Indisponível"
-
-#Enum para Método de pagamento
 class MetodoPagamento(Enum):
-    DINHEIRO = "Dinheiro"
-    CARTAO = "Cartão"
-    PIX = "Pix"
+    DINHEIRO = 'Dinheiro'
+    CARTAO = 'Cartão'
+    PIX = 'PIX'
 
-# Exceção personalizada para horário indisponível
+# -------------------------- EXCEPTION ----------------------------
 class HorarioIndisponivelParaBarbeiro(Exception):
-    def __init__(self):
-        super().__init__("Horário indisponível para o barbeiro")
+    pass
 
-# Classe base pessoa
+# --------------------------- CLASSES -----------------------------
 class Pessoa:
     def __init__(self, cpf, nome, email, telefone, senha):
         self._cpf = cpf
@@ -35,18 +30,18 @@ class Pessoa:
         self._telefone = telefone
         self._senha = senha
 
-# Barbeiro herda de pessoa
 class Barbeiro(Pessoa):
     def __init__(self, cpf, nome, email, telefone, senha, tel_trabalho):
         super().__init__(cpf, nome, email, telefone, senha)
         self._tel_trabalho = tel_trabalho
 
     def verificar_disponibilidade(self, horario):
-        if horario._disponibilidade == Disponibilidade.INDISPONIVEL:
-            raise HorarioIndisponivelParaBarbeiro()
-        return True
+        if horario._disponibilidade != 'Disponível':
+            raise HorarioIndisponivelParaBarbeiro("Horário indisponível para o barbeiro")
 
-# Serviço oferecido
+class Cliente(Pessoa):
+    pass
+
 class Servico:
     def __init__(self, id, nome, descricao, valor_base):
         self._id = id
@@ -54,41 +49,52 @@ class Servico:
         self._descricao = descricao
         self._valor_base = valor_base
 
-# Horário
 class Horario:
-    def __init__(self, id, data, hora, disponibilidade: Disponibilidade):
+    def __init__(self, id, data, hora, disponibilidade):
         self._id = id
         self._data = data
         self._hora = hora
         self._disponibilidade = disponibilidade
 
-# Pagamento
 class Pagamento:
-    def __init__(self, id, data, servico=None, metodo=MetodoPagamento.DINHEIRO):
+    def __init__(self, id, metodo: MetodoPagamento, servico: Servico, adicional=0):
         self._id = id
-        self._data = data
-        self._adicional = 0
         self._metodo = metodo
+        self._adicional = adicional
         self._valor = 0
+        self._definir_valor(servico)
 
-        if servico:
-            self._definir_pagamento(servico)
-
-    def _definir_pagamento(self, servico):
+    def _definir_valor(self, servico):
         self._valor = servico._valor_base + self._adicional
 
-# Agendamento
 class HorarioDeAtendimento:
-    def __init__(self, id, status_servico: StatusServico, status_pagamento: StatusPagamento, confirmado: bool,
-                 justificativa, cliente: Pessoa, barbeiro: Barbeiro, servico: Servico, 
-                 horario: Horario, pagamento: Pagamento = None):
+    def __init__(self, id, status_servico: StatusServico, status_pagamento: StatusPagamento, justificativa, cliente: Cliente, barbeiro: Barbeiro, servico: Servico, horario: Horario, pagamento: Pagamento):
         self._id = id
         self._status_servico = status_servico
         self._status_pagamento = status_pagamento
-        self._confirmado = confirmado
         self._justificativa = justificativa
         self._cliente = cliente
         self._barbeiro = barbeiro
         self._servico = servico
         self._horario = horario
         self._pagamento = pagamento
+
+# ---------------------- FUNÇÕES AUXILIARES ------------------------
+def gerar_id():
+    return str(uuid.uuid4())
+
+def criar_horario_de_atendimento(cliente: Cliente, barbeiro: Barbeiro, horario: Horario, servico: Servico, metodo_pagamento: MetodoPagamento, adicional=0):
+    barbeiro.verificar_disponibilidade(horario)
+    pagamento = Pagamento(gerar_id(), metodo_pagamento, servico, adicional)
+    atendimento = HorarioDeAtendimento(
+        id=gerar_id(),
+        status_servico=StatusServico.AGENDADO,
+        status_pagamento=StatusPagamento.PENDENTE,
+        justificativa=None,
+        cliente=cliente,
+        barbeiro=barbeiro,
+        servico=servico,
+        horario=horario,
+        pagamento=pagamento
+    )
+    return atendimento._id, atendimento._pagamento
