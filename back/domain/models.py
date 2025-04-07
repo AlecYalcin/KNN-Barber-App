2573c16c54b1ccce6d3578e1dc7c68c84bc99426
DennerBismarck <dennerbismarck@gamil.com>
2025-04-07 07:55:35 -0300

    Feat: Criando enums para status e função de verificar horário na classe Barbeiro e expection personalizada #2

--- back/domain/models.py
+++ back/domain/models.py
@@ -1,4 +1,27 @@
-class pessoa():
+from enum import Enum
+
+# Enums para padronizar status
+class StatusServico(Enum):
+    AGENDADO = "Agendado"
+    CONCLUIDO = "Concluído"
+    CANCELADO = "Cancelado"
+
+class StatusPagamento(Enum):
+    PENDENTE = "Pendente"
+    PAGO = "Pago"
+    CANCELADO = "Cancelado"
+
+class Disponibilidade(Enum):
+    DISPONIVEL = "Disponível"
+    INDISPONIVEL = "Indisponível"
+
+# Exceção personalizada para horário indisponível
+class HorarioIndisponivelParaBarbeiro(Exception):
+    def __init__(self):
+        super().__init__("Horário indisponível para o barbeiro")
+
+# Classe base pessoa
+class Pessoa:
     def __init__(self, cpf, nome, email, telefone, senha):
         self._cpf = cpf
         self._nome = nome
@@ -6,40 +29,52 @@ class pessoa():
         self._telefone = telefone
         self._senha = senha
 
-class barbeiro(pessoa):
+# Barbeiro herda de pessoa
+class Barbeiro(Pessoa):
     def __init__(self, cpf, nome, email, telefone, senha, tel_trabalho):
-        #Barbeiro herda os atributos da classe pessoa
         super().__init__(cpf, nome, email, telefone, senha)
         self._tel_trabalho = tel_trabalho
-    
-class servico():
+
+    def verificar_disponibilidade(self, horario):
+        if horario._disponibilidade == Disponibilidade.INDISPONIVEL:
+            raise HorarioIndisponivelParaBarbeiro()
+        return True
+
+# Serviço oferecido
+class Servico:
     def __init__(self, id, nome, descricao, valor_base):
         self._id = id
         self._nome = nome
         self._descricao = descricao
         self._valor_base = valor_base
-        
-class horario():
-    def __init__(self, id, data, hora, disponibilidade):
+
+# Horário
+class Horario:
+    def __init__(self, id, data, hora, disponibilidade: Disponibilidade):
         self._id = id
         self._data = data
         self._hora = hora
         self._disponibilidade = disponibilidade
 
-class pagamento():
+# Pagamento
+class Pagamento:
     def __init__(self, id, data, adicional, metodo, valor):
         self._id = id
-        self._data = data  
+        self._data = data
+        self._adicional = adicional
+        self._metodo = metodo
+        self._valor = valor
 
-class horario_de_atendimento():
-    def __init__(self, id, status_servico, status_pagamento, confirmado, justificativa, cliente, barbeiro, servico, horario, pagamento):
+# Agendamento
+class HorarioDeAtendimento:
+    def __init__(self, id, status_servico: StatusServico, status_pagamento: StatusPagamento, confirmado: bool,
+                 justificativa, cliente: Pessoa, barbeiro: Barbeiro, servico: Servico, 
+                 horario: Horario, pagamento: Pagamento = None):
         self._id = id
         self._status_servico = status_servico
         self._status_pagamento = status_pagamento
         self._confirmado = confirmado
         self._justificativa = justificativa
-        
-        #Classes Externas
         self._cliente = cliente
         self._barbeiro = barbeiro
         self._servico = servico
