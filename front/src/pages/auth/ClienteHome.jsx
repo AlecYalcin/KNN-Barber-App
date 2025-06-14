b3d31690f55b0002c7b06c224e65a2be0a849c43
StenioEric <stenioeric@gmail.com>
2025-06-14 15:19:41 -0300

    feat: adicionada saudação com foto de perfil na página Home #49

---
+++ front/src/pages/auth/ClienteHome.jsx
@@ -0,0 +1,22 @@
+const ClienteHome = () => {
+  return (
+    <div className="mt-15 ml-10 mr-10">
+      <div className="flex ">
+        <div className="w-30 h-30 rounded-full bg-gray-200 mb-3 overflow-hidden border-4 border-blue-100">
+          <img
+            src="https://cdn.creazilla.com/icons/3251108/person-icon-md.png"
+            alt="Foto do cliente"
+          />
+        </div>
+        <div className="ml-6 mt-8">
+          <h1 className="text-3xl text-gray-800">
+            Olá,<br />
+            <strong>João Pedro</strong>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClienteHome;
