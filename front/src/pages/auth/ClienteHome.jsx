178ebad53d4093e26b2524f0d093cb5743fb5092
StenioEric <stenioeric@gmail.com>
2025-06-14 16:03:29 -0300

    feat: adicionada section de cards dos serviços

--- front/src/pages/auth/ClienteHome.jsx
+++ front/src/pages/auth/ClienteHome.jsx
@@ -10,11 +10,33 @@ const ClienteHome = () => {
         </div>
         <div className="ml-6 mt-8">
           <h1 className="text-3xl text-gray-800">
-            Olá,<br />
+            Olá,
+            <br />
             <strong>João Pedro</strong>
           </h1>
         </div>
       </div>
+
+      <div className="mt-10">
+        <h2 className="text-2xl text-gray-800 mb-4">Serviços</h2>
+      </div>
+      <section className="grid grid-cols-2 gap-4">
+        <article className="bg-red-100 shadow-md h-36 rounded-2xl flex items-center justify-center">
+          <h3 className="text-xl font-semibold">Completo</h3>
+        </article>
+
+        <article className="bg-red-100 shadow-md h-36 rounded-2xl flex items-center justify-center">
+          <h3 className="text-xl font-semibold">Barba</h3>
+        </article>
+
+        <article className="bg-red-100 shadow-md h-36 rounded-2xl flex items-center justify-center">
+          <h3 className="text-xl font-semibold">Penteado</h3>
+        </article>
+
+        <article className="bg-red-100 shadow-md h-36 rounded-2xl flex items-center justify-center">
+          <h3 className="text-xl font-semibold">Bigode</h3>
+        </article>
+      </section>
     </div>
   );
 };
