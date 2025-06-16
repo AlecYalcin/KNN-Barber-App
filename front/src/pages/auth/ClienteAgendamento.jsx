2d9337a2e83e116d66cb31563843e9c2e2d5a3b4
StenioEric <stenioeric@gmail.com>
2025-06-15 23:07:24 -0300

    feat(cliente): adicionado layout dos horários disponíveis #50

--- front/src/pages/auth/ClienteAgendamento.jsx
+++ front/src/pages/auth/ClienteAgendamento.jsx
@@ -28,19 +28,53 @@ const ClienteAgendamento = () => {
         </h1>
 
         <div className="grid grid-cols-7 mt-6 bg-ambr-600 gap-2">
-          {Array.from({ length: 31 }, (_, i) => (i + 1).toString()).map((dia) => (
-            <label key={dia} className="cursor-pointer">
-              <input
-                type="radio"
-                name="dia"
-                value={dia}
-                className="hidden peer"
-              />
-              <div className="peer-checked:bg-white h-10 w-10 rounded-2xl border flex items-center justify-center text-2xl font-bold text-gray-800 hover:bg-white hover:text-blue-500 transition">
-                {dia}
-              </div>
-            </label>
-          ))}
+          {Array.from({ length: 31 }, (_, i) => (i + 1).toString()).map(
+            (dia) => (
+              <label key={dia} className="cursor-pointer">
+                <input
+                  type="radio"
+                  name="dia"
+                  value={dia}
+                  className="hidden peer"
+                />
+                <div className="peer-checked:bg-white h-10 w-10 rounded-2xl border flex items-center justify-center text-2xl font-bold text-gray-800 hover:bg-white hover:text-blue-500 transition">
+                  {dia}
+                </div>
+              </label>
+            )
+          )}
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-2xl font-bold mb-2 flex justify-center text-gray-800 mt-10">
+          Horários Disponíveis
+        </h1>
+
+        <div className="w-full bg-ambr-300">
+          <ul className="grid grid-cols-5 gap-2 p-4 rounded-lg">
+            {["08:00", "09:00", "10:00", "11:00", "12:00", "13:00"].map((hora) => (
+              <label key={hora} className=" text-xl rounded-2xl border bg-white h-12 w-15 text-gray-800 font-bold cursor-pointer peer-checked:bg-white peer-checked:text-blue-500">
+                <input
+                  type="checkbox"
+                  className="hidden peer"
+                  aria-label={`Selecionar horário ${hora}`}
+                />
+                <span className="peer-checked:bg-blue-500 peer-checked:text-gray-800 h-full w-full flex items-center justify-center rounded-2xl transition">
+                  {hora}
+                </span>
+              </label>
+            ))}
+          </ul>
+          <div />
+        </div>
+
+        <div className="mt-6 mb-4">
+          <button
+            className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition"
+          >
+            Confirmar
+          </button>
         </div>
       </div>
     </div>
