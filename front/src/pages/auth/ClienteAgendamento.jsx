8207bd854207f4b0e134d81a7abe4618016cceb4
StenioEric <stenioeric@gmail.com>
2025-06-24 09:34:17 -0300

    feat: adicionada responsividade para telas a partir de 1024px #50

--- front/src/pages/auth/ClienteAgendamento.jsx
+++ front/src/pages/auth/ClienteAgendamento.jsx
@@ -3,9 +3,9 @@ import Sidebar from "../../components/SidebarClient";
 
 const ClienteAgendamento = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center py-10 lg:flex-row">
+    <div className="min-h-screen flex flex-col items-center py-10 lg:flex-row mr-5 ml-5">
       <button
-        className="absolute top-8 left-4 flex items-center text-blue-600 hover:text-blue-800 transition focus:outline-none"
+        className="fixed top-8 left-4 z-50 flex items-center text-blue-600 hover:text-blue-800 transition focus:outline-none"
         onClick={() => window.history.back()}
         aria-label="Voltar"
       >
@@ -23,12 +23,12 @@ const ClienteAgendamento = () => {
           />
         </svg>
         Voltar
-      </button>
+      </button> 
 
       <Sidebar />
 
-      <div className="bg-amber-200 lg:ml-64 lg:w-full lg:flex lg:flex-col ">
-        <div className="grid items-center px-5 py-5 rounded-xl mt-10 bg-blue-500">
+      <div className="bg-amer-200 lg:w-full lg:ml-70 lg:flex lg:justify-center lg:items-start lg:h-screen lg:gap-10">
+        <div className="grid items-center px-5 py-5 rounded-xl mt-10 bg-blue-500 lg:justify-center lg:mt-15">
           <h1 className="text-2xl font-bold flex justify-center text-gray-800">
             Agendamento de Serviços
           </h1>
@@ -43,7 +43,7 @@ const ClienteAgendamento = () => {
                     value={dia}
                     className="hidden peer"
                   />
-                  <div className="peer-checked:bg-white h-10 w-10 rounded-2xl border flex items-center justify-center text-2xl font-bold text-gray-800 hover:bg-white hover:text-blue-500 transition">
+                  <div className="peer-checked:bg-white h-10 w-10 lg:h-15 lg:w-15 lg:rounded-3xl lg:text-3xl rounded-2xl border flex items-center justify-center text-2xl font-bold text-gray-800 hover:bg-white hover:text-blue-500 transition">
                     {dia}
                   </div>
                 </label>
@@ -52,14 +52,14 @@ const ClienteAgendamento = () => {
           </div>
         </div>
 
-        <div className="lg:ml-5 ">
-          <h1 className="text-2xl font-bold mb-2 flex justify-center text-gray-800 mt-10">
+        <div className="lg:ml-5 lg:mt-15 bg-amber700">
+          <h1 className="text-2xl font-bold mb-2 flex justify-center text-gray-800 mt-10 lg:mt-0">
             Horários Disponíveis
           </h1>
 
-          <div className="w-full bg-ambr-300">
-            <ul className="grid grid-cols-5 gap-2 p-4 rounded-lg">
-              {["08:00", "09:00", "10:00", "11:00", "12:00", "13:00"].map(
+          <div className="w-full bg-ambr-300 lg:grid justify-center">
+            <ul className="grid grid-cols-5 lg:grid-cols-4 gap-2 p-4 rounded-lg">
+              {["08:00", "09:00", "10:00", "11:00", "12:00", "13:00","14:00","15:00"].map(
                 (hora) => (
                   <label
                     key={hora}
@@ -80,7 +80,7 @@ const ClienteAgendamento = () => {
             <div />
           </div>
 
-          <div className="mt-6 mb-4">
+          <div className="mt-6 mb-4 lg:grid">
             <button className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition">
               Confirmar
             </button>
