6f8e418fd041c8ecbb2a792d023253609ce8bd49
StenioEric <stenioeric@gmail.com>
2025-06-27 20:55:28 -0300

    refactor: excluidas classes desnecessárias e atribuição de novas classes #50

--- front/src/components/SidebarClient.jsx
+++ front/src/components/SidebarClient.jsx
@@ -1,7 +1,7 @@
 const Sidebar = () => {
   return (
-    <div className="hidden lg:grid w-full md:w-64 p-6 lg:fixed lg:left-0 lg:top-20">
-      <div className="flex flex-col items-center mb-8">
+    <div className="hidden lg:flex lg:w-64 lg:flex-col p-6 bg-amer-300 fixed top-8 z-50 lg:h-full shadow-lg rounded-lg">
+      <div className="flex flex-col items-center mb-8 mt-10">
         {/* Foto do perfil */}
         <div className="w-32 h-32 rounded-full bg-gray-200 mb-4 overflow-hidden border-4 border-blue-100">
           <img
@@ -10,10 +10,10 @@ const Sidebar = () => {
             className="w-full h-full object-cover"
           />
         </div>
-        <h2 className="text-2xl font-semibold">Nome do barbeiro</h2>
+        <h2 className="text-2xl font-semibold bg-amer-400">Denner Biscate</h2>
       </div>
 
-      <nav className="space-y-2">
+      <nav className="space-y-2 bg-ambr-600">
         <button className="w-full text-left px-4 py-2 rounded-lg font-medium hover:bg-gray-200">
           Início
         </button>
