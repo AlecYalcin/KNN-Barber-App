575c6a0cb11da6f6a6f4c35163c1c17e1a6db5ac
StenioEric <stenioeric@gmail.com>
2025-07-02 18:54:11 -0300

    feat: adicionada rotas para telas home e agendamento do cliente /  substituição dos button por NavLink

--- front/src/components/SidebarClient.jsx
+++ front/src/components/SidebarClient.jsx
@@ -1,6 +1,8 @@
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
   return (
-    <div className="hidden lg:flex lg:w-64 lg:flex-col p-6 fixed top-17 z-50 lg:h-full shadow-lg">
+    <div className="hidden lg:flex lg:w-64 lg:flex-col p-6 fixed top-17 z-50 lg:h-full shadow-lg bg-white">
       <div className="flex flex-col items-center mb-8 mt-10">
         {/* Foto do perfil */}
         <div className="w-32 h-32 rounded-full bg-gray-200 mb-4 overflow-hidden border-4 border-blue-100">
@@ -10,25 +12,54 @@ const Sidebar = () => {
             className="w-full h-full object-cover"
           />
         </div>
-        <h2 className="text-2xl font-semibold bg-amer-400">Denner Biscate</h2>
+        <h2 className="text-2xl font-semibold text-gray-800">Denner Biscate</h2>
       </div>
 
-      <nav className="space-y-2 bg-ambr-600">
-        <button className="w-full text-left px-4 py-2 rounded-lg font-medium hover:bg-gray-200">
+      <nav className="space-y-2">
+        <NavLink
+          to="/cliente/home"
+          className={({ isActive }) =>
+            `w-full block text-left px-4 py-2 rounded-lg font-medium ${
+              isActive ? "bg-gray-200" : "hover:bg-gray-100"
+            }`
+          }
+          end
+        >
           Início
-        </button>
-        <button className="w-full text-left px-4 py-2 rounded-lg hover:bg-gray-200">
+        </NavLink>
+        <NavLink
+          to="/cliente/agendamento"
+          className={({ isActive }) =>
+            `w-full block text-left px-4 py-2 rounded-lg font-medium ${
+              isActive ? "bg-gray-200" : "hover:bg-gray-100"
+            }`
+          }
+        >
           Agendamentos
-        </button>
-        <button className="w-full text-left px-4 py-2 rounded-lg hover:bg-gray-200">
+        </NavLink>
+        <NavLink
+          to="/pagamentos"
+          className={({ isActive }) =>
+            `w-full block text-left px-4 py-2 rounded-lg font-medium ${
+              isActive ? "bg-gray-200" : "hover:bg-gray-100"
+            }`
+          }
+        >
           Pagamentos
-        </button>
-        <button className="w-full text-left px-4 py-2 rounded-lg bg-gray-200 font-medium">
+        </NavLink>
+        <NavLink
+          to="/cliente/perfil"
+          className={({ isActive }) =>
+            `w-full block text-left px-4 py-2 rounded-lg font-medium ${
+              isActive ? "bg-gray-200" : "hover:bg-gray-100"
+            }`
+          }
+        >
           Perfil
-        </button>
+        </NavLink>
       </nav>
     </div>
   );
-}
+};
 
 export default Sidebar;
