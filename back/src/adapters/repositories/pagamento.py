40dcb8fe15ec9b25c9f2d149f80594fef79f94fe
Guilherme <guimedeiroxdev@gmail.com>
2025-07-09 23:21:55 -0300

    refactor: atualizar métodos do repositório de pagamento para usar 'id' em vez de 'cpf' e adicionar método 'alterar' para atualizações de pagamento #73

--- back/src/adapters/repositories/pagamento.py
+++ back/src/adapters/repositories/pagamento.py
@@ -8,7 +8,7 @@ class AbstractPagamentoRepository():
         raise NotImplementedError
 
     @abc.abstractmethod
-    def remover(self, cpf: str):
+    def remover(self, id: str):
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -23,13 +23,17 @@ class AbstractPagamentoRepository():
     def listar_pagamentos_de_cliente(self, cpf: str) -> list[Pagamento]:
         raise NotImplementedError
 
+    @abc.abstractmethod
+    def alterar(self, id: str, novo_pagamento: Pagamento):
+        raise NotImplementedError
+
 
 class PagamentoRepository(AbstractPagamentoRepository, AbstractSQLAlchemyRepository):
     def adicionar(self, pagamento: Pagamento):
         self.session.add(pagamento)
 
-    def remover(self, cpf: str):
-        pagamento = self.consultar(cpf)
+    def remover(self, id: str):
+        pagamento = self.consultar(id)
         self.session.delete(pagamento)
 
     def consultar(self, id: str) -> Pagamento | None:
@@ -37,8 +41,8 @@ class PagamentoRepository(AbstractPagamentoRepository, AbstractSQLAlchemyReposit
         return pagamento
     
     def consultar_por_agendamento(self, agendamento_id: str) -> Pagamento | None:
-        pagamentos = self.session.query(Pagamento).filter(Pagamento.agendamento_id == agendamento_id).first()
-        return pagamentos
+        pagamento = self.session.query(Pagamento).filter(Pagamento.agendamento_id == agendamento_id).first()
+        return pagamento
 
     def listar_pagamentos_de_cliente(self, cpf: str) -> list[Pagamento]:
         pagamentos = (
@@ -50,3 +54,12 @@ class PagamentoRepository(AbstractPagamentoRepository, AbstractSQLAlchemyReposit
             )
         ).all()
         return pagamentos
+
+    def alterar(self, id: str, novo_pagamento: Pagamento):
+        pagamento = self.consultar(id)
+        if novo_pagamento.valor is not None:
+            pagamento.valor = novo_pagamento.valor
+        if novo_pagamento.metodo is not None:
+            pagamento.metodo = novo_pagamento.metodo
+        if novo_pagamento.data is not None:
+            pagamento.data = novo_pagamento.data
