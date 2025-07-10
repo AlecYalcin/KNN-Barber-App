a4b498088908847364a63e6c2629d31c83447365
StenioEric <stenioeric@gmail.com>
2025-07-10 15:09:14 -0300

    style: ocultar para telas maiores o componente de Perfil #71

--- front/src/components/PhotoPerfil.jsx
+++ front/src/components/PhotoPerfil.jsx
@@ -11,7 +11,7 @@ const PhotoPerfil = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center mb-6">
+    <div className="flex flex-col items-center mb-6 lg:hidden">
       <div className="w-36 h-36 rounded-full bg-gray-200 mb-3 overflow-hidden border-4 border-blue-100">
         <img
           src="https://cdn.creazilla.com/icons/3251108/person-icon-md.png"
